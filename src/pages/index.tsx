961f61d6aea01425f5ba4d5f63a60ef544ca5cb9
bflpa2 <bflp.a2.2023@gmail.com>
2023-03-15 11:47:59 +0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get startedsss by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
