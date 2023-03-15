41495cee86b53767bdd161bd031ef0bc4b209927
bflpa2 <bflp.a2.2023@gmail.com>
2023-03-15 13:27:05 +0700

    tes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Component {...pageProps} />
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3XC7PBC5Q6" />
+    <Script src="/gats.js" />
+    </>
+    )
 }
